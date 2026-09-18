9acb1f8b6722a3a666f032e7f56cfe006b69fa10
Luciusnaris <kizana462@gmail.com>
2026-09-18 18:59:21 +0000

    V1 Successful

--- main.py
+++ main.py
@@ -2,20 +2,63 @@ import random
 
 citizens = [] 
 
+month = 0
+
 def create_name():
     names = ["Alice", "Bob", "Charlie", "David", "Eve"]
     return random.choice(names)
 
-name = create_name()
-
 def create_money():
     return random.randint(100, 5000)
 
-money = create_money()
+def create_age():
+    return random.randint(20, 39)
 
 def create_citizen():
     citizen = {
-        "name": name,
-        "money": money
+        "name": create_name(),
+        "money": create_money(),
+        "age": create_age()
     }
     citizens.append(citizen)
+    print(f"""Citizen added!
+Name: {citizen['name']}
+Age: {citizen['age']}
+Money: {citizen['money']}""")
+
+def aging():     
+    for citizen in citizens:
+        citizen["age"] += 1
+
+def advance_time():
+    global month
+    month += 1
+    if month % 12 == 0:
+        aging()
+
+def display_citizens():
+    for citizen in citizens:
+        print(f"Name: {citizen['name']}, Money: {citizen['money']}, Age: {citizen['age']}")
+        
+def menu():
+    print(f"""===== WORLD SIMULATION =====
+Month : {month}
+
+1. Advance time
+2. Add citizen
+3. Display citizens
+4. Exit
+============================""")
+    choice = input("Enter your choice: ")
+    if choice == "1":
+        advance_time()
+    elif choice == "2":
+        create_citizen()
+    elif choice == "3":
+        display_citizens()
+    elif choice == "4":
+        exit()
+    else:
+        print("Invalid choice. Please try again.")
+while True:
+    menu()  
